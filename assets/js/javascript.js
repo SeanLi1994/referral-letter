59a27ca55c98af17d15c248ada6464f33cc91368
SeanLi1994 <15363540600@163.com>
2021-10-11 12:49:26 +1000

    1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
 
     // Via Query parameters - GET
     const params = (new URL(document.location)).searchParams;
-    const name = params.get('name');
+    const comment = params.get('comment');
     // const surname = params.get('surname');
 
     // Via local Storage
@@ -17,7 +17,29 @@ window.addEventListener('load', () => {
     // const name = sessionStorage.getItem('NAME');
     // const surname = sessionStorage.getItem('SURNAME');
     
-    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-comment').innerHTML = comment;
     // document.getElementById('result-surname').innerHTML = surname;
 
 })
+
+// var formType = $("input[type='radio']:checked").val();
+// if (formType == "form1"){
+//     window.location.replace("/form1.html");
+// }
+// else if (formType == "form2"){
+//     window.location.replace("/form2");
+// }
+// else{
+//     layer.alert("not yet");
+// }
+
+// var formType = $("input[type='radio']:checked").val();
+// if (formType == "form1"){
+//     loginform.action="/letter1";
+// }
+// else if (formType == "form2"){
+//     loginform.action="/letter2";
+// }
+// else{
+//     layer.alert("not yet");
+// }
