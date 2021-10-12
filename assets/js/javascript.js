5f935d5c9edecf757f0dab5dd1a19056b44de620
SeanLi1994 <15363540600@163.com>
2021-10-12 12:58:20 +1000

    1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,8 +2,12 @@ window.addEventListener('load', () => {
 
     // Via Query parameters - GET
     const params = (new URL(document.location)).searchParams;
-    const comment = params.get('comment');
-    const zip = params.get('zip');
+    const comment = params.get('inputComment');
+    const specialistName = params.get('inputSpecialistName');
+    const address1 = params.get('inputAddress');
+    const address2 = params.get('inputAddress2');
+    const state = params.get('inputState');
+    const zip = params.get('inputZip');
 
     // Via local Storage
     /* const name = localStorage.getItem('NAME');
@@ -13,7 +17,11 @@ window.addEventListener('load', () => {
     // const surname = sessionStorage.getItem('SURNAME');
     
     document.getElementById('result-comment').innerHTML = comment;
-    document.getElementById('inputZip').innerHTML = zip;
+    document.getElementById('result-specialistName').innerHTML = specialistName;
+    document.getElementById('result-address1').innerHTML = address1;
+    document.getElementById('result-address2').innerHTML = address2;
+    document.getElementById('result-state').innerHTML = state;
+    document.getElementById('result-zip').innerHTML = zip;
 
 })
 
