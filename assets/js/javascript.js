52fabad1a44474cf9427876de76f21eb09f016e6
SeanLi1994 <15363540600@163.com>
2021-12-04 13:44:53 +1000

    refine

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,6 @@
+// acquire data from the form on Index page
 window.addEventListener('load', () => {
 
-    // Via Query parameters - GET
     const params = (new URL(document.location)).searchParams;
     const comment = params.get('inputComment');
     const specialistName = params.get('inputSpecialistName');
@@ -9,14 +9,8 @@ window.addEventListener('load', () => {
     const state = params.get('inputState');
     const zip = params.get('inputZip');
     const clientObservation = params.get('flexCheckDefault2');
-
-    // Via local Storage
-    /* const name = localStorage.getItem('NAME');
-    const surname = localStorage.getItem('SURNAME'); */
     
-    // const name = sessionStorage.getItem('NAME');
-    // const surname = sessionStorage.getItem('SURNAME');
-    
+// display the above acquired data on the two letter pages
     document.getElementById('result-comment').innerHTML = comment;
     document.getElementById('result-specialistName').innerHTML = specialistName;
     if (document.getElementById('comment-specialistName') != null){
@@ -27,6 +21,7 @@ window.addEventListener('load', () => {
     document.getElementById('result-state').innerHTML = state;
     document.getElementById('result-zip').innerHTML = zip;
 
+// display healthcare data on the Structured Letter page according to the options on the Index page
     if (clientObservation == 'display'){
         $('.clientObservation').show();
     }
